2b5d3951dc558095d4501961b52ea83f5370dabe
David McCandless <33847070+RealityCtrl@users.noreply.github.com>
2018-12-13 14:03:23 +0000

    correcting typo

--- hello_world/generate_download_response.py
+++ hello_world/generate_download_response.py
@@ -1,5 +1,5 @@
 def get_response(file_list_helper, url_helper):
-    file_list = file_list_helper.et_file_list_with_location()
+    file_list = file_list_helper.get_file_list_with_location()
     for file_dict in file_list:
         url = url_helper.get_download_url(file_dict)
         file_dict["Url"] = url
