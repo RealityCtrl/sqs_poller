f9c6ff2b419ba62a67076af0657fb875f3edd240
RealityCtrl <david.mccandless@outlook.com>
2018-12-13 08:53:25 +0000

    yaml fixes and script fix

--- layer_deploy.ps1
+++ layer_deploy.ps1
@@ -1,6 +1,6 @@
-Compress-Archive -Path C:\Users\david\git\polling_lambda\hello_world -DestinationPath C:\tmp\code.zip
+Compress-Archive -Path C:\Users\david\git\polling_lambda\hello_world -DestinationPath C:\tmp\poller_code.zip -Force
 Import-Module AWSPowerShell
 Write-S3Object -BucketName lambda-deployment-realityctrl-sndbx -File C:\tmp\poller_code.zip
 $content = [IO.File]::ReadAllText("C:\Users\david\git\polling_lambda\template.yaml")
 $JSONFile = ConvertFrom-Json "$(get-content $(Join-Path C:\Users\david\git\polling_lambda\ "params.json"))"
-New-CFNStack -StackName mysqshelperlayerstack -TemplateBody $content -Parameter $JSONFile
+New-CFNStack -StackName mypollinglambdastack -Capability CAPABILITY_NAMED_IAM -TemplateBody $content -Parameter $JSONFile
--- layer_update.ps1
+++ layer_update.ps1
@@ -1,6 +1,6 @@
-Compress-Archive -Path C:\Users\david\git\polling_lambda\hello_world -DestinationPath C:\tmp\code.zip
+Compress-Archive -Path C:\Users\david\git\polling_lambda\hello_world -DestinationPath C:\tmp\poller_code.zip -Update
 Import-Module AWSPowerShell
 Write-S3Object -BucketName lambda-deployment-realityctrl-sndbx -File C:\tmp\poller_code.zip
 $content = [IO.File]::ReadAllText("C:\Users\david\git\polling_lambda\template.yaml")
 $JSONFile = ConvertFrom-Json "$(get-content $(Join-Path C:\Users\david\git\polling_lambda\ "params.json"))"
-Update-CFNStack -StackName mysqshelperlayerstack -TemplateBody $content -Parameter $JSONFile
+Update-CFNStack -StackName mypollinglambdastack -Capability CAPABILITY_NAMED_IAM -TemplateBody $content -Parameter $JSONFile
